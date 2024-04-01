e86c84392cbca1348abb5774a3922b4cefce1d28
Alex.Mills <amills@vibeirl.com>
2024-03-31 23:53:25 -0400

    squash-this-commit

---
+++ scripts/git/push.sh
@@ -0,0 +1,24 @@
+
+ssh-add -D
+ssh-add ~/.ssh/id_the1mills
+
+combined=""
+for arg in "${@}"; do
+  combined="${combined} ${arg}"
+done
+
+trimmed="$(echo "$combined" | xargs)"
+
+if test "${trimmed}" == '' ; then
+  trimmed="squash-this-commit";
+fi
+
+git add .
+git add -A
+git commit -am "${trimmed}" || {
+  echo "could not create a new commit"
+}
+
+git push origin || {
+  echo
+}
